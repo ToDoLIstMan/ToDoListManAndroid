9c1c1b9bc0be1855c400fbf06232b0695e48671c
songmho <songmho@gmail.com>
2017-03-21 16:15:23 +0900

    0. firebase 연동. 1. kakao api 연동. 2. splashActivity에 intent추가.

--- app/src/main/java/com/tdl/todolistmanandroid/SplashActivity.java
+++ app/src/main/java/com/tdl/todolistmanandroid/SplashActivity.java
@@ -4,14 +4,27 @@ package com.tdl.todolistmanandroid;
  * Created by HyunWook Kim on 2017-03-21.
  */
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends Activity {
 
+    final int SPLASH_TIME = 2000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                new Intent(SplashActivity.this,MainActivity.class);
+            }
+        },SPLASH_TIME);
+
+
     }
 }
