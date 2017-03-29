5903548a367a2eac6d742991633a26059f61fd87
HyunWook Kim <davkim1030@gmail.com>
2017-03-29 14:34:17 +0900

    SignInActivity.java 수정

---
+++ app/src/main/java/com/tdl/todolistmanandroid/activity/SignInActivity.java
@@ -0,0 +1,50 @@
+package com.tdl.todolistmanandroid.activity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.tdl.todolistmanandroid.R;
+import com.tdl.todolistmanandroid.adapter.MainAdapter;
+import com.tdl.todolistmanandroid.item.MainItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by HyunWook Kim on 2017-03-21.
+ */
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        final EditText textId = (EditText)findViewById(R.id.textId);
+        final EditText textPassword = (EditText)findViewById(R.id.textPassword);
+        Button btLogin = (Button)findViewById(R.id.btLogin);
+        final TextView textSignUp = (TextView)findViewById(R.id.textSignUp);
+        ImageButton kakaoLogin = (ImageButton)findViewById(R.id.kakaoLogin);
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
